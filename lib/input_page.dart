3dee435dcd4353390ce1769aaa63c7a53a31a2c2
abdulaziz-damlahi <abood_d@live.com>
2020-09-12 00:15:59 +0300

    refom widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,59 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: reusable(
+                  colour: Color(0xff1d1e33),
+                ),
+              ),
+              Expanded(
+                child: reusable(
+                  colour: Color(0xff1d1e33),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: reusable(
+              colour: Color(0xff1d1e33),
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: reusable(
+                  colour: Color(0xff1d1e33),
+                ),
+              ),
+              Expanded(
+                child: reusable(
+                  colour: Color(0xff1d1e33),
+                ),
+              )
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class reusable extends StatelessWidget {
+  reusable({@required this.colour});
+  Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
